13672494e2aecd49b9412b2fbeb5cee5fbfb2bf0
anu-ska-911 <anuska.rauniyar@study.lbef.edu.np>
2025-04-24 07:50:19 +0000

    ConversionOperations

---
+++ 03_conversionOperations.js
@@ -0,0 +1,10 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//=== strict check not just number but also data type
