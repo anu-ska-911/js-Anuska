667350083a8d829467b8f89c9e13b6d879be4eef
anu-ska-911 <anuska.rauniyar@study.lbef.edu.np>
2025-04-24 08:56:31 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+// primitive 
+
+// 7 types : string, number, null, boolean, undefined, symbol(unique banauna), BigInt
+
+const score = 100
+const scoreValue = 100.3;
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id == anotherID);
+//reference type(non-primitive)
+
+//Array, Objects, functions, 
+
+const app = ["daraz","Insta","facebook"]
+{
+    name = "anuska",
+    age = 22,
+    email = "anuska@google.com";
+}
+
+const MyFunction = function(){
+    console.log("Hello  JS  users");
+}
+
+console.log(typeof outsideTemp);
