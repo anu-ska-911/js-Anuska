7774c7fb5d1d62d2060c2ec45e4a3681c98759c6
anu-ska-911 <anuska.rauniyar@study.lbef.edu.np>
2025-04-27 03:17:58 +0000

    string

---
+++ string.js
@@ -0,0 +1,31 @@
+const name = "anuska "
+const repoCount = 15
+
+//console.log(name +  repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('anuskaaaarauniyar')
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(toUpperCase());
+
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('k'));
+
+
+const newstring = gameName.substring(1,4);
+console.log(newstring);
+
+
+const anotherString = gameName.slice(0 , 7);
+console.log(anotherString);
+
+
+const newStringone = "    anuskaa      ";
+console.log(newStringone);
+console.log(newStringone.trim());
